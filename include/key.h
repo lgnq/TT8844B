72479bbd3ea0f938f211d2608a3f0c8dd691f9b6
lgnq <dzzxzz@gmail.com>
2017-03-31 22:44:01 +0800

    update BSP

--- include/key.h
+++ include/key.h
@@ -1,7 +1,24 @@
 #ifndef __KEY_H__
 #define __KEY_H__
 
+#include "main.h"
+
+SBIT(KEY_MODE, SFR_P3, 0);
+
+#define KEY_INPUT   KEY_MODE
+
+#define N_KEY       0
+#define S_KEY       1
+#define D_KEY       2
+#define L_KEY       3
+
+#define KEY_STATE_0 0 
+#define KEY_STATE_1 1 
+#define KEY_STATE_2 2 
+#define KEY_STATE_3 3 
+
 void key_init(void);
 void key_update(void);
 
 #endif
+
--- tasks/key.c
+++ tasks/key.c
@@ -1,58 +1,45 @@
-#include "main.h"
 #include "key.h"
-
-#define key_input    KEY_MODE
-
-#define N_key    0
-#define S_key    1
-#define D_key    2
-#define L_key    3
-
-#define key_state_0 0 
-#define key_state_1 1 
-#define key_state_2 2 
-#define key_state_3 3 
  
 unsigned char key_driver(void) 
 { 
-    static unsigned char key_state = key_state_0, key_time = 0; 
-    unsigned char key_press, key_return = N_key; 
+    static unsigned char key_state = KEY_STATE_0, key_time = 0; 
+    unsigned char key_press, key_return = N_KEY; 
 
-    key_press = key_input; 
+    key_press = KEY_INPUT; 
 
     switch (key_state) 
     { 
-        case key_state_0: 
+        case KEY_STATE_0: 
             if (!key_press) 
-                key_state = key_state_1;
+                key_state = KEY_STATE_1;
         break; 
         
-        case key_state_1:               
+        case KEY_STATE_1:               
             if (!key_press) 
             { 
                 key_time = 0;           
-                key_state = key_state_2;
+                key_state = KEY_STATE_2;
             } 
             else 
-                key_state = key_state_0;
+                key_state = KEY_STATE_0;
         break; 
         
-        case key_state_2: 
+        case KEY_STATE_2: 
             if (key_press) 
             { 
-                key_return = S_key;     
-                key_state = key_state_0;
+                key_return = S_KEY;     
+                key_state = KEY_STATE_0;
             } 
             else if (++key_time >= 100) 
             { 
-                key_return = L_key;     
-                key_state = key_state_3;
+                key_return = L_KEY;     
+                key_state = KEY_STATE_3;
             } 
         break; 
 
-        case key_state_3:               
+        case KEY_STATE_3:               
             if (key_press) 
-                key_state = key_state_0;
+                key_state = KEY_STATE_0;
         break; 
     } 
 
@@ -61,35 +48,35 @@ unsigned char key_driver(void)
 
 unsigned char key_read(void) 
 { 
-    static unsigned char key_m = key_state_0, key_time_1 = 0; 
-    unsigned char key_return = N_key, key_temp; 
+    static unsigned char key_m = KEY_STATE_0, key_time_1 = 0; 
+    unsigned char key_return = N_KEY, key_temp; 
       
     key_temp = key_driver(); 
       
     switch (key_m) 
     { 
-        case key_state_0: 
-            if (key_temp == S_key) 
+        case KEY_STATE_0: 
+            if (key_temp == S_KEY) 
             { 
                 key_time_1 = 0;         
-                key_m = key_state_1; 
+                key_m = KEY_STATE_1; 
             } 
             else 
                 key_return = key_temp;  
         break; 
 
-        case key_state_1: 
-            if (key_temp == S_key)      
+        case KEY_STATE_1: 
+            if (key_temp == S_KEY)      
             { 
-                key_return = D_key;     
-                key_m = key_state_0; 
+                key_return = D_KEY;     
+                key_m = KEY_STATE_0; 
             } 
             else                                
             {                            
                 if (++key_time_1 >= 50) 
                 { 
-                    key_return = S_key; 
-                    key_m = key_state_0; 
+                    key_return = S_KEY; 
+                    key_m = KEY_STATE_0; 
                 } 
             } 
         break; 
@@ -108,10 +95,11 @@ void key_update(void)
     
     key = key_read(); 
     
-    if (key == L_key)  
+    if (key == L_KEY)  
         putc('L');
-    else if (key == D_key)  
+    else if (key == D_KEY)  
         putc('D');
-    else if (key == S_key)  
+    else if (key == S_KEY)  
         putc('S');
 }
+
