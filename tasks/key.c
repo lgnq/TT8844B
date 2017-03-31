3f26b538c71cffb04e5ff28cff0bcd8c02a0925f
lgnq <dzzxzz@gmail.com>
2017-03-31 23:32:32 +0800

    only identify short key and long key
    no double-click key

--- tasks/key.c
+++ tasks/key.c
@@ -1,6 +1,6 @@
 #include "key.h"
  
-unsigned char key_driver(void) 
+unsigned char key_read(void) 
 { 
     static unsigned char key_state = KEY_STATE_0, key_time = 0; 
     unsigned char key_press, key_return = N_KEY; 
@@ -44,47 +44,8 @@ unsigned char key_driver(void)
     } 
 
     return key_return; 
-} 
+}
 
-unsigned char key_read(void) 
-{ 
-    static unsigned char key_m = KEY_STATE_0, key_time_1 = 0; 
-    unsigned char key_return = N_KEY, key_temp; 
-      
-    key_temp = key_driver(); 
-      
-    switch (key_m) 
-    { 
-        case KEY_STATE_0: 
-            if (key_temp == S_KEY) 
-            { 
-                key_time_1 = 0;         
-                key_m = KEY_STATE_1; 
-            } 
-            else 
-                key_return = key_temp;  
-        break; 
-
-        case KEY_STATE_1: 
-            if (key_temp == S_KEY)      
-            { 
-                key_return = D_KEY;     
-                key_m = KEY_STATE_0; 
-            } 
-            else                                
-            {                            
-                if (++key_time_1 >= 50) 
-                { 
-                    key_return = S_KEY; 
-                    key_m = KEY_STATE_0; 
-                } 
-            } 
-        break; 
-    }
-
-    return key_return; 
-}     
- 
 void key_init(void)
 {
 }
@@ -97,8 +58,6 @@ void key_update(void)
     
     if (key == L_KEY)  
         putc('L');
-    else if (key == D_KEY)  
-        putc('D');
     else if (key == S_KEY)  
         putc('S');
 }
